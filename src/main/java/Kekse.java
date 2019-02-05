3794fe7f3f7a6384e308c9173336cecde916aac7
Stefan Curow <Stefan.Curow@Bonial.com>
2019-02-05 19:55:44 +0100

    add k8s manifest and health/live urls

--- src/main/java/Kekse.java
+++ src/main/java/Kekse.java
@@ -11,6 +11,16 @@ public class Kekse {
 		return "In need for Kekse!";
 	}
 
+	@RequestMapping("/liveness")
+	String kekse() {
+		return "I am the liveness monster.";
+	}
+
+	@RequestMapping("/readyness")
+	String kekse() {
+		return "I am ready. Hit me.";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Kekse.class, args);
 	}
